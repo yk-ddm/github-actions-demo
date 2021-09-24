6ba46eceed3c8ec97f80a1f6d12c13c70a689ea9
yk-ddm <1715858747@qq.com>
2021-09-24 20:18:44 +0800

    node app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const http = require('http')
-
-http.createServer((req,res)=>{
-    if (req.url == '/') {
-        res.send("hello node ~ yk-ddm")
-    }
-}).listen(3000,()=>{
-	console.log('服务运行了');
-});
+console.log('github actions demo')
